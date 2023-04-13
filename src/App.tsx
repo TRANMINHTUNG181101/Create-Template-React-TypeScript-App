fbb8e665d1f15567c80e3f692c095a2015f4b5e0
Tran Minh Tung <tranminhtung18112001@gmail.com>
2023-04-13 18:26:07 +0700

    Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Tran Minh',
+    last: 'Tung',
+  }
+
+  const nameList = [
+    {
+      first: 'Tran Minh Tung',
+      last: '1',
+    },
+    {
+      first: 'Tran Minh Tung',
+      last: '2',
+    },
+    {
+      first: 'Tran Minh Tung',
+      last: '3',
+    }
+  ]
+
   return (
-    <div className="App">
-      <Greet name="Tung" />
+    <div className='App'>
+      <Greet name='Tung' massageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,18 @@
 type GreetProps = {
     name: string
+    massageCount: number
+    isLoggedIn: boolean
 }
 
 export const Greet = (props: GreetProps) => {
     return(
         <div>
-            <h2>Welcome {props.name}! You have 10 unread messages</h2>
+            <h2>
+                {
+                    props.isLoggedIn ? `Welcome {props.name}! You have {props.massageCount} unread messages` 
+                    : 'Welcome Guest' 
+                }
+            </h2>
         </div>
     )
 }
