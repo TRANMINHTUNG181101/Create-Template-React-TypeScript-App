39bc4e8adb947ffc41dda46b8955d22010165203
Tran Minh Tung <tranminhtung18112001@gmail.com>
2023-04-15 01:35:10 +0700

    useRef Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { UserContextProvider } from './components/context/UserContext';
-import { User } from './components/context/User';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
+      
     </div>
   )
 }
