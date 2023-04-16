bcc18e107211bba49c195e3ace6a5dd371d9787a
Tran Minh Tung <tranminhtung18112001@gmail.com>
2023-04-17 02:25:26 +0700

    Template Literals and Exclude

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import { RandomNumber } from './components/restriction/RandomNumber';
+import { Toast } from './components/templateliterals/Toast';
 
 function App() {
   return (
     <div className='App'>
-      <RandomNumber value={0} isZero/>
+      <Toast position='left-top' />
     </div>
   )
 }
